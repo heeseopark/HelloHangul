96580f55a54ce01e31925706df8d33a107eb7fbf
heeseopark <heeseopark@korea.ac.kr>
2023-11-23 17:34:58 +0900

    Feat: change repository to private

--- ktalk/tests.py
+++ ktalk/tests.py
@@ -1,8 +1,9 @@
 from openai import OpenAI
+from .secrets import OPENAI_API_KEY
 
 class ChatSession:
     def __init__(self):
-        self.client = OpenAI(api_key="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")
+        self.client = OpenAI(api_key=OPENAI_API_KEY)
         self.messages = [{"role": "system", "content": "You are an assistant that provides korean coversation for non-korean users who wants to learn korean. here is the situation: 은행에서 직원한테 계좌개설하는 방법 물어보기. act as if you are having a conversation this the user"}]
 
     def append_message(self, content, role):
--- ktalk/tests2.py
+++ ktalk/tests2.py
@@ -1,6 +1,7 @@
 from openai import OpenAI
+from .secrets import OPENAI_API_KEY
 
-gpt = OpenAI(api_key="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")
+gpt = OpenAI(api_key=OPENAI_API_KEY)
 
 messages = [{"role": "system", "content": "You are an assistant that provides korean coversation for non-korean users who wants to learn korean. here is the situation: 은행에서 직원한테 계좌개설하는 방법 물어보기. act as if you are having a conversation this the user. so use easy korean vocabulary and try to make as many conversations possible."}]
 while True:
